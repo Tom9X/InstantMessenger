a0c92233b9c602c3a1d7290f2da88b606d59d2f0
Paul Gordon <pgordon89@gmail.com>
2013-01-10 17:06:10 -0600

    Server.java class for instant messaging program
    creates streams and sockets for sending and recieving messages, as well
    as the GUI

---
+++ Server.java
@@ -0,0 +1,131 @@
+package buckyintermediate;
+
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/* Whenever you connect to someone else's cpu , you communicate through streams
+ * output stream is sent from your computer to your friend's cpu, input is opposite
+ *  
+ */
+public class Server extends JFrame {
+
+    private JTextField userText;
+    private JTextArea chatWindow;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    private ServerSocket server;
+    private Socket connection;
+
+    //constructor
+    public Server() {
+
+        super("Pauly G's Instant Messenger");
+        userText = new JTextField();
+        userText.setEditable(false);  //doesn't allow user to type while they are not connected to anyone
+        userText.addActionListener(
+                new ActionListener() {
+
+                    public void actionPerformed(ActionEvent event) {
+
+                        sendMessage(event.getActionCommand());// passes the string typed into the text field to the method
+                        userText.setText(""); // reset message area back to blank for you to type new message
+
+                    }
+                });
+        add(userText, BorderLayout.NORTH);
+        chatWindow = new JTextArea();
+        add(new JScrollPane(chatWindow));
+        setSize(300, 150);
+        setVisible(true);
+
+    }
+
+    //sets up and runs the server
+    public void startRunning() {
+        try {
+
+            server = new ServerSocket(6789, 100);
+            while (true) {
+                try {
+                    waitForConnection();
+                    setupStreams();
+                    whileChatting();
+                } catch (EOFException eofException) {
+                    showMessage("\n Server Ended the connection! ");
+                } finally {
+                    closeCrap();
+                }
+            }
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+    }
+
+    //wait for connection, then display connection information
+    private void waitForConnection() throws IOException {
+
+        showMessage("Waiting for someone to connect...\n");
+        connection = server.accept();  //opens port once a person is there to connect to
+        showMessage("Now connected to " + connection.getInetAddress().getHostAddress());
+
+
+    }
+
+    //get stream to send and recieve data
+    private void setupStreams() throws IOException {
+
+        output = new ObjectOutputStream(connection.getOutputStream()); // sets up pathway to send messages out
+        output.flush();
+        input = new ObjectInputStream(connection.getInputStream());  // sets up pathway to recieve messages
+        showMessage("\n Streams are now setup \n");
+    }
+
+    //during the chat
+    private void whileChatting() throws IOException {
+
+        String message = " You are now connected! ";
+        sendMessage(message);
+        ableToType(true);
+        do {
+            try {
+                message = (String) input.readObject();
+                showMessage("\n" + message);
+
+            } catch (ClassNotFoundException classNotFoundException) {
+                showMessage("\n idk what that user sent!");
+            }
+
+        } while (!message.equals("CLIENT - END"));
+    }
+
+    //close streams and sockets after you're done chatting
+    private void closeCrap() {
+        showMessage("\n Closing connections... \n");
+        ableToType(false);
+        try {
+            output.close();
+            input.close();
+            connection.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    //send a message to client
+    private void sendMessage(String message) {
+        try {
+            output.writeObject("SERVER - " + message); // sends them the message
+            output.flush();
+            showMessage("\nSERVER - " + message);  //displays to us our own message
+        } catch (IOException ioException) {
+            chatWindow.append("\n ERROR: Can't send that message! ");
+        }
+    }
+
+
+}
